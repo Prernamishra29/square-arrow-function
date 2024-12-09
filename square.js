5c2e34343af7db379fcd9eac2a6a568d065592d7
91829 <prernamishra453@gmail.com>
2024-12-09 12:38:20 +0530

    Initial commit - Added project files

---
+++ square.js
@@ -0,0 +1,6 @@
+const calculateSquareAndCube = (num) => ({ square: num ** 2, cube: num ** 3 });
+
+// Test cases
+console.log(calculateSquareAndCube(5)); 
+console.log(calculateSquareAndCube(2)); 
+console.log(calculateSquareAndCube(10)); 
